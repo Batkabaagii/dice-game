21acb266c67796e5a9d2db8b9679fcb19d827490
batkabaagii <batkabaagii295@gmail.com>
2021-01-13 08:31:36 -0800

    onoogoo hadgaldag bolgow

--- app.js
+++ app.js
@@ -26,16 +26,27 @@ document.querySelector('.btn-roll').addEventListener('click', function (){
     document.querySelector( ".dice ").style.display = "block"// шоог веб джэр гөргөж ирнэ.
     document.querySelector( ".dice ").src = 'dice-' +diceNumber + ".png";// буусан санамсаргүй тооны зургийг гаргаж ирнээ хө.
     if(diceNumber !== 1 ){
+        total = roundScore + diceNumber;
         roundScore = roundScore + diceNumber;
         document.getElementById('current-' + activePlayer).textContent = roundScore;
     }
     else{  
+        switchToNextPlayer();
+    }
+    
+});
+    document.querySelector('.btn-hold').addEventListener("click", function(){
+        
+        scores[activePlayer] = scores[activePlayer] + roundScore;
+        document.getElementById('score-' + activePlayer ).textContent = scores[activePlayer];
+        switchToNextPlayer();
+        
+    });
+function switchToNextPlayer(){
         roundScore = 0; 
         document.getElementById('current-' + activePlayer).textContent = 0;
         activePlayer === 0 ? (activePlayer = 1):(activePlayer = 0);
-        
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
-        document.querySelector( ".dice ").style.display = "none";
-    }
-});
+        // document.querySelector( ".dice ").style.display = "none";
+}
