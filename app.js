cc3f3722fb7d1f9f0405eab34ae2716a04555b83
batkabaagii <batkabaagii295@gmail.com>
2021-01-12 21:19:36 -0800

    shoog shidewdee hu

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var scores = [0, 0];
 //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог цуглуулах хувьсагч 
 var roundScore = 0;
 //Шооны аль талаараа цуусанг хадгалах хувьсагч хэрэгтэй, 1-6 гэснэ утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.    
- var dice =  Math.floor(Math.random()*6)+1; // Floor бол бутархай оронг нь унагаана шүү андаа хахах +1 гэж байгаа нь 0 гэсэн тоог гаргахгүйн тулд нэмж оруудсан 
+ var diceNumber =  Math.floor(Math.random()*6)+1; // Floor бол бутархай оронг нь унагаана шүү андаа хахах +1 гэж байгаа нь 0 гэсэн тоог гаргахгүйн тулд нэмж оруудсан 
  //document.querySelector('#score-0').textContent = dice; // энд text  мэдээлэл л оруулах боломжтой байдаг.
  //document.querySelector('#score-1').innerHTML = "<em> YES! <EM>"// энд шууд HTML бичиж болно. innerHTML 
  //console.log('Шоо' + dice);
@@ -14,8 +14,14 @@ var roundScore = 0;
  // dice нь class учраас дуудаж ажиллуулахдаа (цэг) тавьж дуудна.
  // одоо эндээс би шоогоо алга болгоно яаж css-ийн dice гэсэн класс дээр display: none; гэж оруулаад алга болноо
 // Эндээс Програм маань эхэллэ шүү найзуудаа
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
+document.getElementById('score-0').textContent = 0;// getElementByID ene bol ID haij bga yumaa hu 
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+var diceDom = document.querySelector( ".dice ");
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+document.querySelector('.btn-roll').addEventListener('click', function (){
+    var diceNumber =  Math.floor(Math.random()*6)+1;
+    document.querySelector( ".dice ").style.display = "block"
+    document.querySelector( ".dice ").src = 'dice-' +diceNumber + ".png";
+});
